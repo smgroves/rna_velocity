c6c500069469fdbfb444c2e8c479cd414518e28d
Sarah Maddox <sarah.f.maddox@vanderbilt.edu>
2019-11-13 19:33:54 -0500

    making loom files

--- velocity_tools/main.py
+++ velocity_tools/main.py
@@ -2,5 +2,6 @@ import numpy as np
 import loompy
 
 from velocity_tools.velocity_utils import read_data
+indir = '/Users/sarahmaddox/Documents/workspace/rna_velocity/output'
 
-read_data("indir")
+read_data(indir,model = 'one_gene',timepoint=0,seed = 32663012)
--- velocity_tools/velocity_utils.py
+++ velocity_tools/velocity_utils.py
@@ -4,18 +4,16 @@ import loompy
 import os.path as op
 import pandas as pd
 
-filename = "test.loom"
-matrix = np.arange(10000).reshape(100,100)
-row_attrs = { "SomeRowAttr": np.arange(100) }
-col_attrs = { "SomeColAttr": np.arange(100) }
-loompy.create(filename, matrix, row_attrs, col_attrs)
-ds = loompy.connect("test.loom")
 
-
-def read_data(indir, model, timepoint, N_cells = 20, delim = ','):
+def read_data(indir, model, timepoint, seed = 0, N_cells = 20, delim = ','):
     #Indir is directory path to output folder
     # model is one_gene, two_gene, etc.
     #timepoint is a single number, starting at 0 for first timepoint
-    read_dir = f"{indir}/{model}/time_{timepoint}"
+    read_dir = f"{indir}/{model}/time_{str(timepoint)}"
     print("Data should have rows = genes, columns = cells, for a single timepoint.")
-    data = pd.read_csv(op.join(read_dir, f's_{N_cells}_cells.csv'), index_col=0, header=0, delimiter=delim)
+    data = pd.read_csv(op.join(read_dir, f's_{N_cells}_cells_{seed}.csv'), index_col=0, header=0, delimiter=delim)
+    filename = f"{seed}.loom"
+    matrix = np.array(data)
+    row_attrs = {"gene": data.index.values}
+    col_attrs = {"cellID": data.columns.values}
+    loompy.create(filename, matrix, row_attrs, col_attrs)
